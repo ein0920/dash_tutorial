c0c66ac6c20699cd4e6913b29b42338a1a1aed57
ein0920 <303224184@qq.com>
2019-03-22 13:38:55 +0800

    part 3 Basic Dash Callbacks: 3e Chained Callbacks

--- app.py
+++ app.py
@@ -7,31 +7,49 @@ external_stylesheets = ['static/css/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+all_options = {
+    'America': ['New York City', 'San Francisco', 'Cincinnati'],
+    'Canada': [u'Montréal', 'Toronto', 'Ottawa']
+}
 app.layout = html.Div([
-    dcc.Input(
-        id='num',
-        type='number',
-        value=5
+    dcc.RadioItems(
+        id='countries-dropdown',
+        options=[{'label': k, 'value': k} for k in all_options.keys()],
+        value='America'
     ),
-    html.Table([
-        html.Tr([html.Td(['x', html.Sup(2)]), html.Td(id='square')]),
-        html.Tr([html.Td(['x', html.Sup(3)]), html.Td(id='cube')]),
-        html.Tr([html.Td([2, html.Sup('x')]), html.Td(id='twos')]),
-        html.Tr([html.Td([3, html.Sup('x')]), html.Td(id='threes')]),
-        html.Tr([html.Td(['x', html.Sup('x')]), html.Td(id='x^x')]),
-    ]),
+
+    html.Hr(),
+
+    dcc.RadioItems(id='cities-dropdown'),
+
+    html.Hr(),
+
+    html.Div(id='display-selected-values')
 ])
 
 
 @app.callback(
-    [Output('square', 'children'),
-     Output('cube', 'children'),
-     Output('twos', 'children'),
-     Output('threes', 'children'),
-     Output('x^x', 'children')],
-    [Input('num', 'value')])
-def callback_a(x):
-    return x**2, x**3, 2**x, 3**x, x**x
+    Output('cities-dropdown', 'options'),
+    [Input('countries-dropdown', 'value')])
+def set_cities_options(selected_country):
+    return [{'label': i, 'value': i} for i in all_options[selected_country]]
+
+
+@app.callback(
+    Output('cities-dropdown', 'value'),
+    [Input('cities-dropdown', 'options')])
+def set_cities_value(available_options):
+    return available_options[0]['value']
+
+
+@app.callback(
+    Output('display-selected-values', 'children'),
+    [Input('countries-dropdown', 'value'),
+     Input('cities-dropdown', 'value')])
+def set_display_children(selected_country, selected_city):
+    return u'{} is a city in {}'.format(
+        selected_city, selected_country,
+    )
 
 
 if __name__ == '__main__':
