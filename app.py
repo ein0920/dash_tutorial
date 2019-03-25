031d527186e1fb4d59b175055865742e9af421d6
ein0920 <303224184@qq.com>
2019-03-25 17:07:32 +0800

    part 6 dcc的语法: 6a_1 scatter 1

--- app.py
+++ app.py
@@ -1,154 +1,58 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import numpy as np
-import pandas as pd
-from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+
 
 external_stylesheets = ['static/css/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-np.random.seed(0)
-df = pd.DataFrame({
-    'Column {}'.format(i): np.random.rand(30) + i*10
-    for i in range(6)})
+app.layout = html.Div(children=[
+    html.H1(children='Dash and Plotly'),
 
-app.layout = html.Div([
-    html.Div(
-        dcc.Graph(
-            id='g1',
-            config={'displayModeBar': False}
-        ), className='four columns'
-    ),
-    html.Div(
-        dcc.Graph(
-            id='g2',
-            config={'displayModeBar': False}
-        ), className='four columns'),
-    html.Div(
-        dcc.Graph(
-            id='g3',
-            config={'displayModeBar': False}
-        ), className='four columns')
-], className='row')
+    html.Div(children='''
+        这里显示Dash和Plotly的画图模块的用法.
+    '''),
 
-
-def highlight(x, y):
-    def callback(*selectedDatas):
-        selectedpoints = df.index
-        for i, selected_data in enumerate(selectedDatas):
-            if selected_data is not None:
-                selected_index = [
-                    p['customdata'] for p in selected_data['points']
-                ]
-                if len(selected_index) > 0:
-                    selectedpoints = np.intersect1d(
-                        selectedpoints, selected_index)
-
-
-        # set which points are selected with the `selectedpoints` property
-        # and style those points with the `selected` and `unselected`
-        # attribute. see
-        # https://medium.com/@plotlygraphs/notes-from-the-latest-plotly-js-release-b035a5b43e21
-        # for an explanation
-
-        figure = {
-            'data': [
-                {
-                    'x': df[x],
-                    'y': df[y],
-                    'text': df.index,
-                    'textposition': 'top',
-                    'selectedpoints': selectedpoints,
-                    'customdata': df.index,
-                    'type': 'scatter',
-                    'mode': 'markers+text',
-                    'marker': {
-                        'color': 'rgba(0, 116, 217, 0.7)',
-                        'size': 12,
-                        'line': {
-                            'color': 'rgb(0, 116, 217)',
-                            'width': 0.5
-                        }
-                    },
-                    'textfont': {
-                        'color': 'rgba(30, 30, 30, 1)'
-                    },
-                    'unselected': {
-                        'marker': {
-                            'opacity': 0.3,
-                        },
-                        'textfont': {
-                            # make text transparent when not selected
-                            'color': 'rgba(0, 0, 0, 0)'
-                        }
-                    }
-                },
+    dcc.Graph(
+        figure=go.Figure(
+            data=[
+                go.Bar(
+                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                    y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                       350, 430, 474, 526, 488, 537, 500, 439],
+                    name='Rest of world',
+                    marker=go.bar.Marker(
+                        color='rgb(55, 83, 109)'
+                    )
+                ),
+                go.Bar(
+                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                       299, 340, 403, 549, 499],
+                    name='China',
+                    marker=go.bar.Marker(
+                        color='rgb(26, 118, 255)'
+                    )
+                )
             ],
-            'layout': {
-                'clickmode': 'event+select',
-                'margin': {'l': 15, 'r': 0, 'b': 15, 't': 5},
-                'dragmode': 'select',
-                'hovermode': 'closest',
-                'showlegend': False
-            }
-        }
-
-        # Display a rectangle to highlight the previously selected region
-        shape = {
-            'type': 'rect',
-            'line': {
-                'width': 1,
-                'dash': 'dot',
-                'color': 'darkgrey'
-            }
-        }
-        if selectedDatas[0] and selectedDatas[0]['range']:
-            figure['layout']['shapes'] = [dict({
-                'x0': selectedDatas[0]['range']['x'][0],
-                'x1': selectedDatas[0]['range']['x'][1],
-                'y0': selectedDatas[0]['range']['y'][0],
-                'y1': selectedDatas[0]['range']['y'][1]
-            }, **shape)]
-        else:
-            figure['layout']['shapes'] = [dict({
-                'type': 'rect',
-                'x0': np.min(df[x]),
-                'x1': np.max(df[x]),
-                'y0': np.min(df[y]),
-                'y1': np.max(df[y])
-            }, **shape)]
-
-        return figure
-
-    return callback
-
-
-
-# app.callback is a decorator which means that it takes a function
-# as its argument.
-# highlight is a function "generator": it's a function that returns function
-app.callback(
-    Output('g1', 'figure'),
-    [Input('g1', 'selectedData'),
-     Input('g2', 'selectedData'),
-     Input('g3', 'selectedData')]
-)(highlight('Column 0', 'Column 1'))
-
-app.callback(
-    Output('g2', 'figure'),
-    [Input('g2', 'selectedData'),
-     Input('g1', 'selectedData'),
-     Input('g3', 'selectedData')]
-)(highlight('Column 2', 'Column 3'))
-
-app.callback(
-    Output('g3', 'figure'),
-    [Input('g3', 'selectedData'),
-     Input('g1', 'selectedData'),
-     Input('g2', 'selectedData')]
-)(highlight('Column 4', 'Column 5'))
+            layout=go.Layout(
+                title='US Export of Plastic Scrap',
+                showlegend=True,
+                legend=go.layout.Legend(
+                    x=0,
+                    y=1.0
+                ),
+                margin=go.layout.Margin(l=40, r=0, t=40, b=30)
+            )
+        ),
+        style={'height': 300},
+        id='my-graph'
+    )
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
