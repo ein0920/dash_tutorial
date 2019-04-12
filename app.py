b97f920552cfef68db9b258adf2b06e1529d0e81
ein0920 <303224184@qq.com>
2019-04-12 14:01:14 +0800

    part 8 DashTable: 8a Quickstart

--- app.py
+++ app.py
@@ -1,58 +1,16 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.graph_objs as go
+import dash_table
+import pandas as pd
 
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/solar.csv')
 
-external_stylesheets = ['static/css/bWLwgP.css']
+app = dash.Dash(__name__)
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.H1(children='Dash and Plotly'),
-
-    html.Div(children='''
-        这里显示Dash和Plotly的画图模块的用法.
-    '''),
-
-    dcc.Graph(
-        figure=go.Figure(
-            data=[
-                go.Bar(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
-                       350, 430, 474, 526, 488, 537, 500, 439],
-                    name='Rest of world',
-                    marker=go.bar.Marker(
-                        color='rgb(55, 83, 109)'
-                    )
-                ),
-                go.Bar(
-                    x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
-                       2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-                    y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
-                       299, 340, 403, 549, 499],
-                    name='China',
-                    marker=go.bar.Marker(
-                        color='rgb(26, 118, 255)'
-                    )
-                )
-            ],
-            layout=go.Layout(
-                title='US Export of Plastic Scrap',
-                showlegend=True,
-                legend=go.layout.Legend(
-                    x=0,
-                    y=1.0
-                ),
-                margin=go.layout.Margin(l=40, r=0, t=40, b=30)
-            )
-        ),
-        style={'height': 300},
-        id='my-graph'
-    )
-])
+app.layout = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in df.columns],
+    data=df.to_dict("rows"),
+)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
