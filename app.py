b7bf1ff9fcd9d3449019d47f72239a81cf894322
ein0920 <303224184@qq.com>
2019-04-22 10:44:53 +0800

    part 8 DashTable: 8f-1 Editable DataTable Predefined Columns

--- app.py
+++ app.py
@@ -1,88 +1,50 @@
 import dash
 from dash.dependencies import Input, Output
-import dash_html_components as html
 import dash_table
-from dash_table.Format import Format, Scheme, Sign, Symbol
+import dash_core_components as dcc
+import dash_html_components as html
 import pandas as pd
-from collections import OrderedDict
 
 app = dash.Dash(__name__)
 
-df_typing_formatting = pd.DataFrame(OrderedDict([
-    ('city', ['NYC', 'Montreal', 'Los Angeles']),
-    ('max', [108, 99, 111]),
-    ('max_date', ['1926-07-22', '1975-08-01', '1939-09-20']),
-    ('min', [-15, -36, 28]),
-    ('min_date', ['1934-02-09', '1957-01-15', '1913-01-07'])
-]))
+params = [
+    'Weight', 'Torque', 'Width', 'Height',
+    'Efficiency', 'Power', 'Displacement'
+]
 
 app.layout = html.Div([
     dash_table.DataTable(
-        id='typing_formatting',
-        data=df_typing_formatting.to_dict('rows'),
-        columns=[{
-            'id': 'city',
-            'name': 'City',
-            'type': 'text'
-        }, {
-            'id': 'max',
-            'name': u'Max Temperature (˚F)',
-            'type': 'numeric',
-            'format': Format(
-                precision=0,
-                scheme=Scheme.fixed,
-                symbol=Symbol.yes,
-                symbol_suffix=u'˚F'
-            ),
-            # equivalent manual configuration
-            # 'format': {
-            #     'locale': {
-            #         'symbol': ['', '˚F']
-            #     },
-            #     'specifier': '$.0f'
-            # }
-        }, {
-            'id': 'max_date',
-            'name': 'Max Temperature (Date)',
-            'type': 'datetime'
-        }, {
-            'id': 'min',
-            'name': u'Min Temperature (˚F)',
-            'type': 'numeric',
-            'format': Format(
-                nully='N/A',
-                precision=0,
-                scheme=Scheme.fixed,
-                sign=Sign.parantheses,
-                symbol=Symbol.yes,
-                symbol_suffix=u'˚F'
-            ),
-            # equivalent manual configuration
-            # 'format': {
-            #     'locale': {
-            #         'symbol': ['', '˚F']
-            #     },
-            #     'nully': 'N/A'
-            #     'specifier': '($.0f'
-            # }
-            'on_change': {
-                'action': 'coerce',
-                'failure': 'default'
-            },
-            'validation': {
-                'default': None
-            }
-        }, {
-            'id': 'min_date',
-            'name': 'Min Temperature (Date)',
-            'type': 'datetime',
-            'on_change': {
-                'action': 'none'
-            }
-        }],
+        id='table-editing-simple',
+        columns=(
+            [{'id': 'Model', 'name': 'Model'}] +
+            [{'id': p, 'name': p} for p in params]
+        ),
+        data=[
+            dict(Model=i, **{param: 0 for param in params})
+            for i in range(1, 5)
+        ],
         editable=True
-    )
+    ),
+    dcc.Graph(id='table-editing-simple-output')
 ])
+
+
+@app.callback(
+    Output('table-editing-simple-output', 'figure'),
+    [Input('table-editing-simple', 'data'),
+     Input('table-editing-simple', 'columns')])
+def display_output(rows, columns):
+    df = pd.DataFrame(rows, columns=[c['name'] for c in columns])
+    return {
+        'data': [{
+            'type': 'parcoords',
+            'dimensions': [{
+                'label': col['name'],
+                'values': df[col['id']]
+            } for col in columns]
+        }]
+    }
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
