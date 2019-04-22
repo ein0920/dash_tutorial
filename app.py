de24a477663adc8659063a265289b54bd65b744a
ein0920 <303224184@qq.com>
2019-04-22 10:54:00 +0800

    part 8 DashTable: 8g-2 DataTable Dropdowns DataTable with Per-Row Dropdowns

--- app.py
+++ app.py
@@ -8,50 +8,79 @@ from collections import OrderedDict
 
 app = dash.Dash(__name__)
 
-df = pd.DataFrame(OrderedDict([
-    ('climate', ['Sunny', 'Snowy', 'Sunny', 'Rainy']),
-    ('temperature', [13, 43, 50, 30]),
-    ('city', ['NYC', 'Montreal', 'Miami', 'NYC'])
+df_per_row_dropdown = pd.DataFrame(OrderedDict([
+    ('City', ['NYC', 'Montreal', 'Los Angeles']),
+    ('Neighborhood', ['Brooklyn', 'Mile End', 'Venice']),
+    ('Temperature (F)', [70, 60, 90]),
 ]))
 
 
 app.layout = html.Div([
     dash_table.DataTable(
-        id='table-dropdown',
-        data=df.to_dict('rows'),
+        id='dropdown_per_row',
+        data=df_per_row_dropdown.to_dict('rows'),
         columns=[
-            {'id': 'climate', 'name': 'climate', 'presentation': 'dropdown'},
-            {'id': 'temperature', 'name': 'temperature'},
-            {'id': 'city', 'name': 'city', 'presentation': 'dropdown'},
+            {'id': 'City', 'name': 'City'},
+            {'id': 'Neighborhood', 'name': 'Neighborhood', 'presentation': 'dropdown'},
+            {'id': 'Temperature (F)', 'name': 'Temperature (F)'}
         ],
 
         editable=True,
-        column_static_dropdown=[
+        column_conditional_dropdowns=[
             {
-                'id': 'climate',
-                'dropdown': [
-                    {'label': i, 'value': i}
-                    for i in df['climate'].unique()
+                # column id
+                'id': 'Neighborhood',
+                'dropdowns': [
+                    {
+                        # these are filter strings
+                        'condition': 'City eq "NYC"',
+                        'dropdown': [
+                            {'label': i, 'value': i}
+                            for i in [
+                                'Brooklyn',
+                                'Queens',
+                                'Staten Island'
+                            ]
+                        ]
+                    },
+
+                    {
+                        'condition': 'City eq "Montreal"',
+                        'dropdown': [
+                            {'label': i, 'value': i}
+                            for i in [
+                                'Mile End',
+                                'Plateau',
+                                'Hochelaga'
+                            ]
+                        ]
+                    },
+
+                    {
+                        'condition': 'City eq "Los Angeles"',
+                        'dropdown': [
+                            {'label': i, 'value': i}
+                            for i in [
+                                'Venice',
+                                'Hollywood',
+                                'Los Feliz'
+                            ]
+                        ]
+                    }
+
                 ]
-            },
-            {
-                'id': 'city',
-                'dropdown': [
-                    {'label': i, 'value': i}
-                    for i in df['city'].unique()
-                ]
-            },
+            }
         ]
     ),
-    html.Div(id='table-dropdown-container')
+    html.Div(id='dropdown_per_row_container')
 ])
 
 
 # In order for the changes in the dropdown to persist,
 # the dropdown needs to be "connected" to the table via
 # a callback
-@app.callback(Output('table-dropdown-container', 'children'),
-              [Input('table-dropdown', 'data_timestamp')])
+@app.callback(Output('dropdown_per_row_container', 'children'),
+              [Input('dropdown_per_row', 'data_timestamp')])
 def update_output(timestamp):
     return timestamp
 
