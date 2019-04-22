2d373d1daa957df4efeeaa14f158270c3de0cfa7
ein0920 <303224184@qq.com>
2019-04-22 10:59:05 +0800

    part 8 DashTable: 8i-1 DataTable Filtering Syntax Frontend Filtering

--- app.py
+++ app.py
@@ -1,39 +1,49 @@
+'''
+As discussed in the interactivity chapter, DataTable includes filtering capabilities.
+Users can turn on filtering options by defining the filtering attribute.
+filtering=True will initiate clientside (frontend) filtering.
+Alternatively you can specify filtering='fe'.
+If the DataTable is quite large, clientside filtering will likely become slow.
+Using the backend filtering option: filtering='be' will allow serverside filtering.
+At this time, the filter syntax for frontend and backend filtering differs slightly.
+'''
+
 import dash
 from dash.dependencies import Input, Output
 import dash_table
+import dash_html_components as html
+
 import pandas as pd
+
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
 
 app = dash.Dash(__name__)
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/Emissions%20Data.csv').reset_index()
-df['Emission'] = df['Emission'].map(lambda x: '{0:.2f}'.format(x))
-
-app.layout = dash_table.DataTable(
-        id='table-virtualiztion',
-        data=df.to_dict('rows'),
+app.layout = html.Div([
+    dash_table.DataTable(
+        id='datatable-filtering-fe',
         columns=[
-            {'name': i, 'id': i} for i in df.columns
+            {"name": i, "id": i, "deletable": True} for i in df.columns
         ],
-        n_fixed_rows=1,
-        style_cell={
-            'whiteSpace': 'normal'
-        },
-        style_data_conditional=[
-            {'if': {'column_id': 'index'},
-             'width': '50px'},
-            {'if': {'column_id': 'Year'},
-             'width': '50px'},
-            {'if': {'column_id': 'Country'},
-             'width': '100px'},
-            {'if': {'column_id': 'Continent'},
-             'width': '70px'},
-            {'if': {'column_id': 'Emission'},
-             'width': '75px'},
-        ],
-        virtualization=True,
-        pagination_mode=False
-)
+        data=df.to_dict("rows"),
+        filtering=True,
+    ),
+    html.Div(id='datatable-filter-container')
+])
+
+
+@app.callback(
+    Output('datatable-filter-container', "children"),
+    [Input('datatable-filtering-fe', "data")])
+def update_graph(rows):
+    if rows is None:
+        dff = df
+    else:
+        dff = pd.DataFrame(rows)
+
+    return html.Div()
 
 
 if __name__ == '__main__':
