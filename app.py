3db0cbc162b90ee3238cfdafb7d56f28a71f87b7
ein0920 <303224184@qq.com>
2019-04-22 10:50:56 +0800

    part 8 DashTable: 8f-6 Editable DataTable Updating Columns of the Same Table

--- app.py
+++ app.py
@@ -8,75 +8,29 @@ import pandas as pd
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    html.Div([
-        dcc.Input(
-            id='adding-rows-name',
-            placeholder='Enter a column name...',
-            value='',
-            style={'padding': 10}
-        ),
-        html.Button('Add Column', id='adding-rows-button', n_clicks=0)
-    ], style={'height': 50}),
-
     dash_table.DataTable(
-        id='adding-rows-table',
-        columns=[{
-            'name': 'Column {}'.format(i),
-            'id': 'column-{}'.format(i),
-            'deletable': True,
-            'editable_name': True
-        } for i in range(1, 5)],
-        data=[
-            {'column-{}'.format(i): (j + (i-1)*5) for i in range(1, 5)}
-            for j in range(5)
+        id='computed-table',
+        columns=[
+            {'name': 'Input Data', 'id': 'input-data'},
+            {'name': 'Input Squared', 'id': 'output-data'}
         ],
+        data=[{'input-data': i} for i in range(11)],
         editable=True,
-        row_deletable=True
     ),
-
-    html.Button('Add Row', id='editing-rows-button', n_clicks=0),
-
-    dcc.Graph(id='adding-rows-graph')
 ])
 
 
 @app.callback(
-    Output('adding-rows-table', 'data'),
-    [Input('editing-rows-button', 'n_clicks')],
-    [State('adding-rows-table', 'data'),
-     State('adding-rows-table', 'columns')])
-def add_row(n_clicks, rows, columns):
-    if n_clicks > 0:
-        rows.append({c['id']: '' for c in columns})
+    Output('computed-table', 'data'),
+    [Input('computed-table', 'data_timestamp')],
+    [State('computed-table', 'data')])
+def update_columns(timestamp, rows):
+    for row in rows:
+        try:
+            row['output-data'] = float(row['input-data']) ** 2
+        except:
+            row['output-data'] = 'NA'
     return rows
-
-
-@app.callback(
-    Output('adding-rows-table', 'columns'),
-    [Input('adding-rows-button', 'n_clicks')],
-    [State('adding-rows-name', 'value'),
-     State('adding-rows-table', 'columns')])
-def update_columns(n_clicks, value, existing_columns):
-    if n_clicks > 0:
-        existing_columns.append({
-            'id': value, 'name': value,
-            'editable_name': True, 'deletable': True
-        })
-    return existing_columns
-
-
-@app.callback(
-    Output('adding-rows-graph', 'figure'),
-    [Input('adding-rows-table', 'data'),
-     Input('adding-rows-table', 'columns')])
-def display_output(rows, columns):
-    return {
-        'data': [{
-            'type': 'heatmap',
-            'z': [[row.get(c['id'], None) for c in columns] for row in rows],
-            'x': [c['name'] for c in columns]
-        }]
-    }
 
 
 if __name__ == '__main__':
