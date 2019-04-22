4dc25a7033498a4de8b94bc8f0751adb223f4a74
ein0920 <303224184@qq.com>
2019-04-22 10:26:17 +0800

    part 8 DashTable: 8d-1 DataTable - Python Callbacks Backend Paging

--- app.py
+++ app.py
@@ -1,96 +1,40 @@
 import dash
 from dash.dependencies import Input, Output
 import dash_table
-import dash_core_components as dcc
 import dash_html_components as html
+import dash_core_components as dcc
 import pandas as pd
+
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
+df[' index'] = range(1, len(df) + 1)
+
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": i, "id": i, "deletable": True} for i in df.columns
-        ],
-        data=df.to_dict("rows"),
-        editable=True,
-        filtering=True,
-        sorting=True,
-        sorting_type="multi",
-        row_selectable="multi",
-        row_deletable=True,
-        selected_rows=[],
-        pagination_mode="fe",
-            pagination_settings={
-                "displayed_pages": 1,
-                "current_page": 0,
-                "page_size": 35,
-            },
-            navigation="page",
-    ),
-    html.Div(id='datatable-interactivity-container')
-])
+PAGE_SIZE = 5
+
+app.layout = dash_table.DataTable(
+    id='datatable-paging',
+    columns=[
+        {"name": i, "id": i} for i in sorted(df.columns)
+    ],
+    pagination_settings={
+        'current_page': 0,
+        'page_size': PAGE_SIZE
+    },
+    pagination_mode='be'
+)
+
 
 @app.callback(
-    Output('datatable-interactivity-container', "children"),
-    [Input('datatable-interactivity', "derived_virtual_data"),
-     Input('datatable-interactivity', "derived_virtual_selected_rows")])
-def update_graph(rows, derived_virtual_selected_rows):
-    # When the table is first rendered, `derived_virtual_data` and
-    # `derived_virtual_selected_rows` will be `None`. This is due to an
-    # idiosyncracy in Dash (unsupplied properties are always None and Dash
-    # calls the dependent callbacks when the component is first rendered).
-    # So, if `rows` is `None`, then the component was just rendered
-    # and its value will be the same as the component's dataframe.
-    # Instead of setting `None` in here, you could also set
-    # `derived_virtual_data=df.to_rows('dict')` when you initialize
-    # the component.
-    if derived_virtual_selected_rows is None:
-        derived_virtual_selected_rows = []
-
-    if rows is None:
-        dff = df
-    else:
-        dff = pd.DataFrame(rows)
-
-    colors = []
-    for i in range(len(dff)):
-        if i in derived_virtual_selected_rows:
-            colors.append("#7FDBFF")
-        else:
-            colors.append("#0074D9")
-
-    return html.Div(
-        [
-            dcc.Graph(
-                id=column,
-                figure={
-                    "data": [
-                        {
-                            "x": dff["country"],
-                            # check if column exists - user may have deleted it
-                            # If `column.deletable=False`, then you don't
-                            # need to do this check.
-                            "y": dff[column] if column in dff else [],
-                            "type": "bar",
-                            "marker": {"color": colors},
-                        }
-                    ],
-                    "layout": {
-                        "xaxis": {"automargin": True},
-                        "yaxis": {"automargin": True},
-                        "height": 250,
-                        "margin": {"t": 10, "l": 10, "r": 10},
-                    },
-                },
-            )
-            for column in ["pop", "lifeExp", "gdpPercap"]
-        ]
-    )
-
+    Output('datatable-paging', 'data'),
+    [Input('datatable-paging', 'pagination_settings')])
+def update_graph(pagination_settings):
+    return df.iloc[
+        pagination_settings['current_page']*pagination_settings['page_size']:
+        (pagination_settings['current_page'] + 1)*pagination_settings['page_size']
+    ].to_dict('rows')
 
 
 if __name__ == '__main__':
